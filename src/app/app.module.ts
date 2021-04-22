0558d351e7d36695a567f3fe07bd188e8891ad4d
massoudyan1 <47738720+massoudyan1@users.noreply.github.com>
2021-04-22 09:31:55 +0200

    123

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './before-login/welcome/welcome.component';
+import { AboutComponent } from './before-login/welcome/components/about/about.component';
+import { ContactComponent } from './before-login/welcome/components/contact/contact.component';
+import { NavComponent } from './before-login/welcome/components/nav/nav.component';
+import { SigninComponent } from './before-login/welcome/components/signin/signin.component';
+import { SignupComponent } from './before-login/welcome/components/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    AboutComponent,
+    ContactComponent,
+    NavComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
