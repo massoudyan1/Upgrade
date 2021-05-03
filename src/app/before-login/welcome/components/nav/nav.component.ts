f107a1dc33765f0fd5ca0ae6e7a464c6f97dfccb
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2021-05-03 08:45:08 +0200

    Update nav.component.ts

--- src/app/before-login/welcome/components/nav/nav.component.ts
+++ src/app/before-login/welcome/components/nav/nav.component.ts
@@ -7,14 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { 
-
+  constructor() {
   }
 
   ngOnInit(): void {
 
   }
-
-
-  
 }
