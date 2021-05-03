152c75ea86b48562dd1491d5b02ced8ee8ebfb6f
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2021-05-03 08:17:24 +0200

    Update app-routing.module.ts
    Added more paths.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './before-login/welcome/components/about/about.component';
 import { ContactComponent } from './before-login/welcome/components/contact/contact.component';
+import { SigninComponent } from './before-login/welcome/components/signin/signin.component';
+import { SignupComponent } from './before-login/welcome/components/signup/signup.component';
 
 
 const routes: Routes = [
-  { path: 'about', component: AboutComponent },
-  {path: 'contact', component: ContactComponent}
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'signin', component: SigninComponent},
+  {path: 'signup', component: SignupComponent}
 ];
 
 @NgModule({
