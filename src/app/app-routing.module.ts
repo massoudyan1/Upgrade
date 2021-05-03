b27efc41f3cf5d4b02e22977824c1ffcedcbe86b
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2021-05-03 08:15:25 +0200

    Update app-routing.module.ts
    Added contact path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './before-login/welcome/components/about/about.component';
+import { ContactComponent } from './before-login/welcome/components/contact/contact.component';
 
 
 const routes: Routes = [
-  { path: 'about', component: AboutComponent }
+  { path: 'about', component: AboutComponent },
+  {path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
