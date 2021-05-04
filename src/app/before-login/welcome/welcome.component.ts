b6eb25144ecf291b4d69b6b78604af1810280328
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2021-05-04 07:24:35 +0200

    Update welcome.component.ts

--- src/app/before-login/welcome/welcome.component.ts
+++ src/app/before-login/welcome/welcome.component.ts
@@ -7,8 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  title = 'Upgrade';
 
+  constructor() { }
   ngOnInit(): void {
   }
 
