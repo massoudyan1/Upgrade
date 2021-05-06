3f69969e4ee67693b6ab5526ec343b90bcf30358
Lukas Stauersbøl <lukas.stauersbol@gmail.com>
2021-05-06 14:54:02 +0200

    Changes

--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthService } from './auth.service';
+import { NgAuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let service: NgAuthService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    service = TestBed.inject(NgAuthService);
   });
 
   it('should be created', () => {
