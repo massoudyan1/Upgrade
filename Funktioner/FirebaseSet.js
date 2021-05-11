07a470224a25c7c57b0e4a640830a635d30f1ab8
massoudyan1 <47738720+massoudyan1@users.noreply.github.com>
2021-05-11 12:29:37 +0200

    Set & Update
    Examples

--- Funktioner/FirebaseSet.js
+++ Funktioner/FirebaseSet.js
@@ -13,11 +13,9 @@ const db = firebase.firestore();
 
 db.settings({ timestampInSnapshots: true })
 
-db.collection('collecion').doc('id').update({
-    name: 'new name',
-    city: 'another city'
+db.collection('collection').doc('id').set({
+    heigth: 'new height'
 });
 
-// Update kan ikke overwrite data, 
-// hvis man tilføjer et nyt dokument til kollektionen
-// (kan ikke add en kulonne til en tabel)
+
+// Set overwriter data, så man kan lave nye documenter, som f.eks height
