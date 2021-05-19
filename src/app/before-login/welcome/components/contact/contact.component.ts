ed5afe6afe4e0969f9938fc57e1b7f72b6331486
Lukas lukst. Stauersbøl <lukas.stauersbol@gmail.com>
2021-05-19 08:27:35 +0200

    Fixes major issues and still has some work.

--- src/app/before-login/welcome/components/contact/contact.component.ts
+++ src/app/before-login/welcome/components/contact/contact.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
-import { MessageService } from 'primeng/api/public_api';
-import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-contact',
@@ -11,8 +11,8 @@ import { HttpClient } from 'selenium-webdriver/http';
 export class ContactComponent implements OnInit {
 
   contactForm: FormGroup;
-  submitted: boolean = false;
-  isLoading: boolean = false;
+  submitted = false;
+  isLoading = false;
   name: FormControl = new FormControl('', [Validators.required]);
   email: FormControl = new FormControl('', [Validators.required, Validators.email]);
   message: FormControl = new FormControl('', [Validators.required, Validators.maxLength(256)]);
@@ -28,5 +28,48 @@ export class ContactComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+  onSubmit() {
+    if (this.contactForm.status == 'VALID' && this.honeypot.value == '') {
+      this.contactForm.disable();
+      const formData:FormData = new FormData();
+      formData.append('name', this.contactForm.get('name').value);
+      formData.append('email', this.contactForm.get('email').value);
+      formData.append('message', this.contactForm.get('message').value);
+      this.isLoading = true;
+      this.submitted = false;
+      this.http.post('URL FROM GOOGLE HERE', formData).subscribe(
+        (response) => {
+          if (response == 'success') {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Success',
+              detail: 'Den mail er blevet sendt.'
+            });
+          } else {
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Error',
+              detail: 'Afsendelsen af din mail fik en fejl.'
+            });
+          }
+          this.contactForm.enable();
+          this.submitted = true;
+          this.isLoading = false;
+          console.log(response);
+        },
+        (error) => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: 'En fejl er opstået, prøv og genindlæs siden igen eller prøv senere.'
+          });
+          this.contactForm.enable();
+          this.submitted = true;
+          this.isLoading = false;
+          console.log(error);
+        }
+      );
+    }
   }
 }
