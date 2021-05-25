16d7c1ae4f3835db8de9d8d612dab46d1b3b8851
Lukas lukst. Stauersbøl <lukas.stauersbol@gmail.com>
2021-05-25 12:45:26 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class AppComponent {
   title = 'Upgrade';
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor(private primengConfig: PrimeNGConfig) { }
 
   ngOnInit() {
     this.primengConfig.ripple = true;
