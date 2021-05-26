086e59c83bcb4a6c5c0466c9328292ede84413f1
massoudyan1 <47738720+massoudyan1@users.noreply.github.com>
2021-05-26 08:12:53 +0200

    ng g c beskeder
    i after login

---
+++ src/app/after-login/beskeder/beskeder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-beskeder',
+  templateUrl: './beskeder.component.html',
+  styleUrls: ['./beskeder.component.scss']
+})
+export class BeskederComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
