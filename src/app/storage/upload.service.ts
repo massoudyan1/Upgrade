a2108db8aaae214bc1946773d693ec3b5b8186d7
Lukas lukst. Stauersbøl <lukas.stauersbol@gmail.com>
2021-05-26 14:48:56 +0200

    Update upload.service.ts

--- src/app/storage/upload.service.ts
+++ src/app/storage/upload.service.ts
@@ -1,35 +1,34 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
+import firebase from 'firebase/app'
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseUploadService {
-  location = 'uploads/';
-  constructor(private angularFireStorage: AngularFireStorage) { }
+  private basePath = '/images';
+  file: File;
+  url = '';
 
-  /* Image name generator time */
-  imageName() {
-    const newTime = Math.floor(Date.now() / 1000);
-    return Math.floor(Math.random() * 20) + newTime;
+  constructor(private afStorage: AngularFireStorage) { }
+
+  handleFiles(event) {
+    this.file = event.target.files[0];
   }
 
-  async storeImage(imageData: any) {
-    try {
-      const imageName = this.imageName();
-      return new Promise((resolve, reject) => {
-        const pictureRef = this.angularFireStorage.ref(this.location + imageName);
-        pictureRef
-          .put(imageData)
-          .then(function () {
-            pictureRef.getDownloadURL().subscribe((url: any) => {
-              resolve(url);
-            });
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      });
-    } catch (e) { }
+  //method to upload file at firebase storage
+  async uploadFile() {
+    if (this.file) {
+      const filePath = `${this.basePath}/${this.file.name}`;    //path at which image will be stored in the firebase storage
+      const snap = await this.afStorage.upload(filePath, this.file);    //upload task
+      this.getUrl(snap);
+    } else {alert('Please select an image'); }
+  }
+
+  //method to retrieve download url
+  private async getUrl(snap: firebase.storage.UploadTaskSnapshot) {
+    const url = await snap.ref.getDownloadURL();
+    this.url = url;  //store the URL
+    console.log(this.url);
   }
 }
