ec742da6198dc7f0716a367555390d496fd7800d
Lukas lukst. Stauersbøl <lukas.stauersbol@gmail.com>
2021-05-27 07:41:53 +0200

    Small changes and package update

--- src/app/storage/upload.service.ts
+++ src/app/storage/upload.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
 import firebase from 'firebase/app'
+import { User } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseUploadService {
-  private basePath = '/images';
+  constructor(private afStorage: AngularFireStorage, private user: User) { }
+  iUser = this.user.uid
+  private basePath = `/uploads/images/${this.iUser}`;
   file: File;
   url = '';
 
-  constructor(private afStorage: AngularFireStorage) { }
-
-  handleFiles(event) {
-    this.file = event.target.files[0];
-  }
-
   //method to upload file at firebase storage
-  async uploadFile() {
+  async uploadFile(event) {
+    this.file = event.target.files[0];
     if (this.file) {
       const filePath = `${this.basePath}/${this.file.name}`;    //path at which image will be stored in the firebase storage
       const snap = await this.afStorage.upload(filePath, this.file);    //upload task
