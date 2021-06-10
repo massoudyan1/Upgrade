6368072ad6ba74b4ff2defa3d0248a5c5c91dbe6
Slash1y <lukas.stauersbol@gmail.com>
2021-06-10 08:08:49 +0200

    fixed prod mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA5Zoqz5Zim18mBm9L9lpwrM1qMGnPQNKE',
+    authDomain: 'upgrade-47108.firebaseapp.com',
+    projectId: 'upgrade-47108',
+    storageBucket: 'upgrade-47108.appspot.com',
+    messagingSenderId: '1013623221779',
+    appId: '1:1013623221779:web:455bb63b9bf0648de0d9e6'
+  }
 };
